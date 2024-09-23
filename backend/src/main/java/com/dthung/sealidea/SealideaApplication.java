22ae903961e96739f2d115b89e7014641751d233
dthung99 <dthung.y17@gmail.com>
2024-09-23 01:38:03 +0100

    update remember me key

--- backend/src/main/java/com/dthung/sealidea/SealideaApplication.java
+++ backend/src/main/java/com/dthung/sealidea/SealideaApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
-public class SapinionApplication {
+public class SealideaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SapinionApplication.class, args);
+		SpringApplication.run(SealideaApplication.class, args);
 	}
 
 	public ForwardedHeaderFilter forwardedHeaderFilter() {

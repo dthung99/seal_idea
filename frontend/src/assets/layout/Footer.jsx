d380f47b31697aede760eaabb7e5c734aff03d58
dthung99 <dthung.y17@gmail.com>
2024-09-28 15:17:05 +0100

    update footer website

--- frontend/src/assets/layout/Footer.jsx
+++ frontend/src/assets/layout/Footer.jsx
@@ -9,8 +9,9 @@ const Footer = () => {
         <div className="footer_main">
           <h4>Get in Touch</h4>
           <p>
-            LinkedIn: <a href="https://linkedin.com/in/dthung" target="_blank" rel="noopener noreferrer">linkedin.com/in/dthung</a> |
-            GitHub: <a href="https://github.com/dthung99" target="_blank" rel="noopener noreferrer">github.com/dthung99</a>
+            My&nbsp;website:&nbsp;<a href="https://dthung.com" target="_blank" rel="noopener noreferrer">dthung.com</a> |
+            LinkedIn:&nbsp;<a href="https://linkedin.com/in/dthung" target="_blank" rel="noopener noreferrer">linkedin.com/in/dthung</a> |
+            GitHub:&nbsp;<a href="https://github.com/dthung99" target="_blank" rel="noopener noreferrer">github.com/dthung99</a>
           </p>
           <p>
             Email: <a href="mailto:the_hung.dang@kcl.ac.uk">the_hung.dang@kcl.ac.uk</a>, <a href="mailto:dthung.y17@gmail.com">dthung.y17@gmail.com</a> | London, United Kingdom
